646ebd2c090679f3938abd742c79fd46040df1e8
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-24 22:20:45 +0700

    Create ai.py

---
+++ modules/ai.py
@@ -0,0 +1,10 @@
+def ai_help(text):
+
+    if "python" in text:
+        print("Tip: Use python3 filename.py")
+
+    elif "git" in text:
+        print("Tip: clone repository with 'clone URL'")
+
+    else:
+        print("AI help not available")
