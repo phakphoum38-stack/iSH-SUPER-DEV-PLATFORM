565b4d47b17af8f0b493c4612da529129b79385b
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-24 22:16:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from cli import start_cli
+
+def banner():
+    print("====================================")
+    print("        SUPER DEV PLATFORM")
+    print("====================================")
+
+def main():
+    banner()
+    start_cli()
+
+if __name__ == "__main__":
+    main()
